6f4959d95a80dfc94eb21fcee1991135344aa822
nuresctor <escamillatorresnuria@gmail.com>
2024-04-17 17:28:48 +0200

    first logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
-
+import { StateContext } from '@/conext/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-
+    <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
-  
+    </StateContext>
   )
 }
 
